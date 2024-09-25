5098e582041b242ea88be340057127895c3ba353
Hoelac0ste <Hoelacoste@gmail.com>
2024-09-25 21:08:06 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -4,7 +4,14 @@ def get_multiplied_digits(number):
     if len(str_number) == 1:
         return number
     else:
+        a = len(str_number)
+        b = str_number[a - 1]
+        if int(b) == 0:
+            str_number = str_number[0:a - 1]
+        else:
+            str_number = str_number
+            
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
-    
-print(get_multiplied_digits(102034))
+
+print(get_multiplied_digits(42050))
